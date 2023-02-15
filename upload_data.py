7964d22314832a98819d51daf5ffd17950173dc6
RobinJehn <robin.jehn@gmx.de>
2023-02-15 13:27:41 +0000

    use table name

--- upload_data.py
+++ upload_data.py
@@ -5,23 +5,20 @@ import re
 from datetime import datetime
 import sys
 
-ser = serial.Serial("COM7",9600)
-server = 'sql8.freemysqlhosting.net'
-database = 'sql8596986'
-username = 'sql8596986'
-password = 'zzRR3mNM8j'
+ser = serial.Serial("COM7", 9600)
+server = "sql8.freemysqlhosting.net"
+database = "sql8596986"
+username = "sql8596986"
+password = "zzRR3mNM8j"
 
 connection = mysql.connector.connect(
-    host=server,
-    database=database, 
-    user=username,
-    password=password
+    host=server, database=database, user=username, password=password
 )
 if connection.is_connected():
     db_Info = connection.get_server_info()
     print("Connected to MySQL Server version ", db_Info)
     cursor = connection.cursor()
-    query = """SELECT * FROM pressure_sensor_data"""
+    query = """SELECT * FROM sensor_data"""
 
 else:
     sys.exit("Can't connect to db")
@@ -31,9 +28,9 @@ values = dict()
 while True:
     try:
         s = ser.readline().decode().strip()
-        d = re.search(r'reading(\d) = (\d+)', s).groups()  # Sensor Id, pressure
+        d = re.search(r"reading(\d) = (\d+)", s).groups()  # Sensor Id, pressure
         t = time.time()
-        readable_t = datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+        readable_t = datetime.utcfromtimestamp(t).strftime("%Y-%m-%d %H:%M:%S")
         values[d[0]] = (d[0], t, d[1])
     except Exception as e:
         print(e)
@@ -42,12 +39,10 @@ while True:
     # Only push data once we have collected one datapoint per sensor
     if len(values.keys) == 8:
         for _, value in values.items():
-            string= f"INSERT INTO table (sensor_id, time_of_reading, reading) VALUES({value[0]}, \"{value[1]}\", {value[2]}) ON DUPLICATE KEY UPDATE time_of_reading=\"{value[1]}\", reading={value[2]};"
+            string = f'INSERT INTO sensor_data (sensor_id, time_of_reading, reading) VALUES({value[0]}, "{value[1]}", {value[2]}) ON DUPLICATE KEY UPDATE time_of_reading="{value[1]}", reading={value[2]};'
             print(string)
             cursor.execute(string)
 
         connection.commit()
         values = dict()
         time.sleep(1)
-    
-
