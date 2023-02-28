603469b5c09d62fdec8afb977b0f555c2c63c936
RobinJehn <s2024553@ed.ac.uk>
2023-02-28 20:02:33 +0000

    train, evaluate and save model

--- posture_classifier.py
+++ posture_classifier.py
@@ -1,19 +1,15 @@
 import tensorflow as tf
 import numpy as np
 
-x_train = np.array(
-    [
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-)
-y_train = np.array([1, 1, 1, 1, 0, 0, 0, 0])
+good_x_train = np.load("good.npy")
+bad_x_train = np.load("bad.npy")
+
+good_y_train = np.ones(len(good_x_train))
+bad_y_train = np.zeros(len(bad_x_train))
+
+x_train = np.append(good_x_train, bad_x_train, axis=0)
+y_train = np.append(good_y_train, bad_y_train, axis=0)
+
 
 model = tf.keras.models.Sequential(
     [
@@ -28,3 +24,17 @@ loss_fn = tf.keras.losses.BinaryCrossentropy()
 model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
 
 model.fit(x_train, y_train, epochs=10)
+
+good_x_test = np.load("good_test.npy")
+bad_x_test = np.load("bad_test.npy")
+
+good_y_test = np.ones(len(good_x_test))
+bad_y_test = np.zeros(len(bad_x_test))
+
+x_test = np.append(good_x_test, bad_x_test, axis=0)
+y_test = np.append(good_y_test, bad_y_test, axis=0)
+
+print("Model performance:")
+model.evaluate(x_test, y_test)
+
+model.save("models/posture-model.h5", overwrite=True, include_optimizer=True)
